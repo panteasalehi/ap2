b9400469fcd03ed66ac5dfd16c7d135dd0951459
pantea salehi <pantolia.salehi@gmail.com>
2021-03-10 11:02:16 +0330

    id added

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args){
         System.out.println("Hi git");
         System.out.println("My name is: <>");
-        System.out.println("My student ID is <>");
+        System.out.println("My student ID is 99243046");
     }
 }
